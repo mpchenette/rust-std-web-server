102c397f44b225cb52fb4322823cd61fd8d2df86
Matthew Chenette <matthew@chenette.com>
2023-05-29 21:15:31 -0500

    working!

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,89 +1,82 @@
+
+// things not in std: log, regex, tls
+
 use std::{
     io::{Read, Write},
-    str::SplitWhitespace,
 };
 
-struct Http {
-    request_method: String,
-    request_url: String,
-    request_version: String,
+struct HttpRequest {
+    method: String,
+    url: String,
+    version: String,
 }
 
-// Returns an http struct if valid, panic if not
-fn is_valid_http(mut request: &std::net::TcpStream) -> Http {
-    let mut data: [u8; 5000] = [0 as u8; 5000];//String = String::new();
-    match request.read(&mut data) {
-        Ok(size) => {
-            let s: &str = match std::str::from_utf8(&data[0..size]) {
-                Ok(v) => v,
-                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-            };
-            // println!("attempting to split, {}", data);
-            let mut request_items: SplitWhitespace = s.split_whitespace();
-            // println!("just split, {}", request_items.next().unwrap().to_string());
+const DOCKER: bool = false;
+const SITE_PATH: &str = "site/";
 
-            let req: Http = Http {
-                request_method: request_items.next().unwrap().to_string(),
-                request_url: request_items.next().unwrap().to_string(),
-                request_version: request_items.next().unwrap().to_string(),
+fn is_valid_http(mut tcp_stream: &std::net::TcpStream) -> HttpRequest {
+    println!("\nIS_VALID_HTTP\n-------------");
+    let mut buffer: [u8; 5000] = [0 as u8; 5000];
+    match tcp_stream.read(&mut buffer) {
+        Ok(size) => {
+            let str_to_split: &str = match std::str::from_utf8(&buffer[0..size]) {
+                Ok(str) => str,
+                Err(e) => panic!("Invalid UTF-8: {}", e)
             };
-            return req;
+            let mut request_items: std::str::SplitWhitespace = str_to_split.split_whitespace();
+            let http_request: HttpRequest = HttpRequest {
+                method: request_items.next().unwrap().to_string(),
+                url: request_items.next().unwrap().to_string(),
+                version: request_items.next().unwrap().to_string()
+            };
+            println!("returning:\n   method: {}\n   url: {}\n   version: {}\n", http_request.method, http_request.url, http_request.version);
+            return http_request;
         }
-        Err(_) => {
-            request.shutdown(std::net::Shutdown::Both).unwrap(); // Do I need to shutdown the tcpstream???
-            panic!("Invalid HTTP Request. Shutting down the TcpStream.");
-        } //panic if invalid. only accept valid http requests
-          // should i panic if I'm in a thread? does that even work?
+        Err(e) => {
+            tcp_stream.shutdown(std::net::Shutdown::Both).unwrap(); // Do I need to shutdown the tcpstream???
+            panic!("Invalid HTTP Request. Shutting down the TcpStream. Error: {}", e); // should i panic if I'm in a thread? does that even work?
+        }
     }
 }
 
 fn handle_tcp_stream(mut tcp_stream: std::net::TcpStream) {
-    let req: Http = is_valid_http(&tcp_stream);
-    let mut url: String = String::from("site"); //String::from("/index.html"); // is this (and the below) the best way to do this?
-    if req.request_url.eq("/") {
-        url.push_str("/index.html");
-    } else {
-        url.push_str(&(req.request_url)); // not sure if it's faster/more efficint to set this in an else or just set it initially then overwrite if index
-    }
-    let file_contents: Vec<u8> = match std::fs::read(&url) {
+    println!("\nHANDLE_TCP_STREAM\n-----------------");
+    let http_request: HttpRequest = is_valid_http(&tcp_stream);
+    let file_path: String = if http_request.url.eq("/") { String::from("index.html") } else { http_request.url };
+    let file_contents: Vec<u8> = match std::fs::read(format!("{}{}", SITE_PATH, &file_path)) {
         Ok(v) => v,
-        Err(_) => std::fs::read("site/404.html").unwrap(),
+        Err(_) => std::fs::read(format!("{}{}", SITE_PATH, "404.html")).unwrap()
     };
     match tcp_stream.write(&file_contents) {
-        Ok(size) => {
-            println!(
-                "TcpStream Write Success: {} (of size {} bytes) written to {}",
-                url,
-                size,
-                tcp_stream.local_addr().unwrap()
-            )
-        }
-        Err(e) => {
-            println!("TcpStream Write Error: {}", e)
-        }
-    }
+        Ok(size) => println!("TcpStream Write Success: {} (of size {} bytes) written to {}", file_path, size, tcp_stream.local_addr().unwrap()),
+        Err(e) => println!("TcpStream Write Error: {}", e)
+    };
 }
 
 fn main() {
-    let tcp_listener: std::net::TcpListener = std::net::TcpListener::bind("127.0.0.1:80").unwrap(); // https://stackoverflow.com/questions/66725777/how-to-connect-to-rust-server-app-that-runs-on-docker
+    let addr: &str = if DOCKER { "0.0.0.0:80" } else { "127.0.0.1:80" }; // https://stackoverflow.com/questions/66725777/how-to-connect-to-rust-server-app-that-runs-on-docker
+    let tcp_listener: std::net::TcpListener = match std::net::TcpListener::bind(addr){
+        Ok(tcp_listener) => tcp_listener,
+        Err(e) => panic!("Unable to bind to {}. Error: {}", addr, e),
+    };
     println!("Listening on {}", tcp_listener.local_addr().unwrap());
     for tcp_stream in tcp_listener.incoming() {
         match tcp_stream {
             Ok(tcp_stream) => {
                 // https://doc.rust-lang.org/std/thread/index.html#spawning-a-thread
                 let thread_join_handle: std::thread::JoinHandle<()> = std::thread::spawn(move || handle_tcp_stream(tcp_stream));
-                let res = thread_join_handle.join();
+                let join_result: Result<(), Box<dyn std::any::Any + Send>> = thread_join_handle.join();
 
                 // https://doc.rust-lang.org/std/thread/type.Result.html#examples
-                match res {
-                    Ok(_) => {},//println!("spawn succeeded"),
-                    Err(e) => std::panic::resume_unwind(e),
+                match join_result {
+                    Ok(_) => println!("join succeeded"),
+                    Err(e) => std::panic::resume_unwind(e)
                 }
             }
             Err(e) => {
-                println!("TcpStream Error: {}", e);
+                println!("TcpStream Error: {}", e)
             }
         }
     }
-    std::mem::drop(tcp_listener); // TODO: core::mem vs. std::mem?
+    std::mem::drop(tcp_listener);
 }
