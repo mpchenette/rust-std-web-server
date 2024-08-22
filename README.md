0cea7b3cfaeab8299f03ff5f21bd84ea02e0b6fa
Matthew Chenette <mpchenette@github.com>
2024-08-22 15:16:11 -0500

    WORKING - about to change http request parsing

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Docker image that is ~5 MB in size.
 - Add to CI/CD
     - OWASP ZAP
     - SonarCloud
-      - SonarCLoud does not support rust but we can use cargo clippy and cargo tarpaulin
+      - SonarCLoud does not support rust but we can use cargo clippy and cargo tarpaulin. cargo audit?
 - Rust: things not in std: log, regex, tls
 - enable binding to both IPv4 and IPv6 in main.rs
 - convert all current print statements into logs (4/2024 - can't do this in native rust. only println exists)
@@ -20,6 +20,7 @@ Docker image that is ~5 MB in size.
 - change the tcp_stream.read to tcp_stream.read_to_end now that we have a timeout
   - scratch the above. use bufreader instead
 - look into if it's best practice to have main() return Result<(), Box<dyn Error>> or std::io::Result<()>
+- be sure we are handling header injection attacks when ingesting HTTP requests
 
 ## How to run the server
 
@@ -50,7 +51,7 @@ Last, regardless of which step you took above, navigate to `127.0.0.1:8000` in y
 
 ### Run on Azure App Service
 
-To run on an Azure App Service, ensure that there is an environment varialbe (App Setting) set to the following:
+To run on an Azure App Service, ensure that there is an environment variable (App Setting) set to the following:
 
 ```
 WEBSITES_PORT=8000
@@ -61,3 +62,10 @@ To build a rust web server (not framework) that uses only the base rust crates, 
 
 ## Known issues
 The Docker build does not seem to work on Apple silicon as I'm not sure there is a way to compile static rust binaries on apple silicon. I'm thinking [this is the issue](https://stackoverflow.com/questions/76618704/is-it-possible-to-static-link-the-rust-application-when-using-apple-m1).
+
+## Explanation of decisions
+1. No uses of `panic!` or `expect`, instead we use graceful error handling with `Result` as it is more appropriate for production level code
+
+1. We use the `?` operator to propagate errors (when possible) instead of match statements as they are more concise and easier to read.
+
+1. When creating the `TcpListener` in main, we explicitly use a `SocketAddr` (as oppose to just providing a string) as it is the more explicit approach. If you provide a string (&str) or other allowed value, it will create and use a `SocketAddr` behind the scenes anyway, so let's bring that logic to the front and make it visible. [Test](server/src/main.rs#L20)
--- server/src/main.rs
+++ server/src/main.rs
@@ -2,21 +2,28 @@
 
 mod tcp;
 mod thread;
+mod copilot_http_request_validation;
 
-const DOCKER: bool = true;
+// const DOCKER: bool = true;
 
 // Create a TcpListener (on the server) to handle TcpStreams (i.e., connections from clients)
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("LOG (MAIN): Starting server");
 
-    // Determine which IPv4 address we will attempt to bind to - https://stackoverflow.com/questions/66725777/how-to-connect-to-rust-server-app-that-runs-on-docker
-    let address: &str = if DOCKER { "0.0.0.0:8000" } else { "127.0.0.1:8000" };
+    // // Determine which IPv4 address we will attempt to bind to - https://stackoverflow.com/questions/66725777/how-to-connect-to-rust-server-app-that-runs-on-docker
+    // let ipv4_address: &str = if DOCKER {
+    //     "0.0.0.0:8000"
+    // } else {
+    //     "127.0.0.1:8000"
+    // }; // 08AUG2024: is this needed? on my mac, when DOCKER = true, I can still connect to 0.0.0.0:8000 from localhost:8000
 
-    // Create the TcpListener by binding to the determined address
-    let tcp_listener: std::net::TcpListener = match std::net::TcpListener::bind(address) {
-        Ok(tcp_listener) => tcp_listener,
-        Err(e) => panic!("ERROR (MAIN): Unable to bind to {}. Error: {}", address, e),
-    };
+    // TODO: should we only allow IPv6 or allow IPv4 too?
+    let ipv6_addr: std::net::Ipv6Addr = std::net::Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1);
+    let port: u16 = 8000;
+    let ipv6_socket_addr: std::net::SocketAddrV6 = std::net::SocketAddrV6::new(ipv6_addr, port, 0, 0);
+
+    // Create the TcpListener by binding to the determined socket address (i.e., ip + port)
+    let tcp_listener: std::net::TcpListener = std::net::TcpListener::bind(ipv6_socket_addr)?; // Anything that implements std::net::ToSocketAddrs can be provided here. This includes a &str, a full-blown Socket Addr like I do here, or even a (ipv6addr, port). See, https://doc.rust-lang.org/std/net/trait.ToSocketAddrs.html for more info.
 
     // Log the address that the TcpListener is listening on
     match tcp_listener.local_addr() {
@@ -31,15 +38,16 @@ fn main() {
     for tcp_stream in tcp_listener.incoming() {
         match tcp_stream {
             Ok(tcp_stream) => {
-
                 // Log the address of the connected TcpStream (client)
                 match tcp_stream.local_addr() {
-                    Ok(local_addr) => println!("\nLOG (MAIN): New TcpStream Received ({})", local_addr),
+                    Ok(local_addr) => {
+                        println!("\nLOG (MAIN): New TcpStream Received ({})", local_addr)
+                    }
                     Err(e) => println!("WARNING (MAIN): Failed to log the local address: {}", e),
                 }
 
                 // Handle the TcpStream (connection) using a thread from the thread pool
-                pool.execute(/* move? */|| tcp::handle_tcp_stream(tcp_stream));
+                pool.execute(/* move? */ || tcp::handle_tcp_stream(tcp_stream));
             }
             Err(e) => {
                 println!("ERROR (MAIN): TcpStream Error: {}", e)
@@ -47,4 +55,6 @@ fn main() {
         }
     }
     std::mem::drop(tcp_listener);
+
+    Ok(())
 }
